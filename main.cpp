4743adbb03383608104e484b2a621093a3f41714
Ian Jarvis <ijarvis12@gmail.com>
2022-10-01 17:15:26 -0500

    new tests

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
   BigNum i1 = BigNum();
   cout << i1 << endl;
 
-  BigNum i2 = BigNum("10101010");
+  BigNum i2 = BigNum("1010101010");
   cout << i2 << endl;
 
   BigNum i3 = BigNum(1000000100);
@@ -16,22 +16,22 @@ int main(){
   cout << endl;
 
   //Operator Tests
-  BigNum t1 = BigNum("2222") + BigNum("9999");
+  BigNum t1 = BigNum("999999999") + BigNum("1");
   cout << t1 << endl;
 
-  BigNum t2 = BigNum("10000") - BigNum("9999");
+  BigNum t2 = BigNum("100000000") - BigNum("99999999");
   cout << t2 << endl;
 
-  BigNum t3 = BigNum("99999") * BigNum("333");
+  BigNum t3 = BigNum("999999999") * BigNum("333");
   cout << t3 << endl;
 
-  BigNum t4 = pow(BigNum("2"),20);
+  BigNum t4 = pow(BigNum("2"),30);
   cout << t4 << endl;
 
-  BigNum t5 = BigNum("99999") / BigNum("333");
+  BigNum t5 = BigNum("999999999") / BigNum("333");
   cout << t5 << endl;
 
-  BigNum t6 = BigNum("99999") % BigNum("333");
+  BigNum t6 = BigNum("999999999") % BigNum("333");
   cout << t6 << endl;
 
   return 0;
