2da75c49e74ef3694cd76452b761fbf6b5f29a41
Ian Jarvis <ijarvis12@gmail.com>
2023-09-11 13:13:34 -0500

    Update main.cpp
    bignum to bigint

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
-#include "bignum.h"
+#include "bigint.h"
 using namespace std;
 
 int main(){
   //Initializer Tests
-  BigNum i1 = BigNum();
+  BigInt i1 = BigInt();
   cout << i1 << endl;
 
-  BigNum i2 = BigNum("-12345678901234567890");
+  BigInt i2 = BigInt("-12345678901234567890");
   cout << i2 << endl;
 
-  BigNum i3 = BigNum(-1234567890);
+  BigInt i3 = BigInt(-1234567890);
   cout << i3 << endl;
 
   cout << endl;
 
   //Operator Tests
-  BigNum t1 = BigNum("999999999") + BigNum("1");
+  BigInt t1 = BigInt("999999999") + BigInt("1");
   cout << t1 << endl;
 
-  BigNum t2 = BigNum("1000000000") - BigNum("999999999");
+  BigInt t2 = BigInt("1000000000") - BigInt("999999999");
   cout << t2 << endl;
 
-  BigNum t3 = BigNum("9999999999") * BigNum("333");
+  BigInt t3 = BigInt("9999999999") * BigInt("333");
   cout << t3 << endl;
 
-  BigNum t4 = pow(BigNum("2"),36);
+  BigInt t4 = pow(BigInt("2"),36);
   cout << t4 << endl;
 
-  BigNum t5 = BigNum("9999999999") / BigNum("333");
+  BigInt t5 = BigInt("9999999999") / BigInt("333");
   cout << t5 << endl;
 
-  BigNum t6 = BigNum("9999999999999999") % BigNum("3333333333");
+  BigInt t6 = BigInt("9999999999999999") % BigInt("3333333333");
   cout << t6 << endl;
 
   return 0;
