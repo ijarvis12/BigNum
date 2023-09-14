7147602b046724a6a5e8c5d707e27f1005fb76d7
Ian Jarvis <ijarvis12@gmail.com>
2023-09-14 14:00:33 -0500

    Create bigdecimal.h

---
+++ bigdecimal.h
@@ -0,0 +1,248 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "bigint.h"
+using namespace std;
+
+class BigDecimal{
+  private:
+    BigInt value;
+    BigInt scale;
+  public:
+    BigDecimal()
+    BigDecimal(string num);
+    BigDecimal(double num);
+    BigDecimal(BigInt v, BigInt s);
+    ~BigDecimal();
+    void trim();
+    unsigned long int precision();
+    //Getters
+    BigInt getValue();
+    BigInt getScale();
+    //Setters
+    void setValue();
+    void setScale();
+};
+
+BigDecimal operator +(const BigDecimal& first, const BigDecimal& second);
+BigDecimal operator -(const BigDecimal& first, const BigDecimal& second);
+BigDecimal operator *(const BigDecimal& first, const BigDecimal& second);
+BigDecimal operator /(const BigDecimal& first, const BigDecimal& second);
+BigDecimal operator %(const BigDecimal& first, const BigDecimal& second);
+BigDecimal pow(const BigDecimal& a, const unsigned long int n);
+BigDecimal pow(const BigDecimal& a, const BigInt& n);
+
+BigDecimal::BigDecimal(){
+  this->value = BigInt("0");
+  this->scale = BigInt("0");
+};
+
+BigDecimal::BigDecimal(string num){
+  unsigned long int dec_pos = num.find('.', 1);
+  if(dec_pos > 1 && dec_pos < num.length()){
+    long int s = num.length() - dec_pos - 1;
+    string v = num.replace(dec_pos, 1, '');
+    this->value = BigInt(v);
+    this->scale = BigInt(s);
+  }
+  else if(dec_pos == 0) *this = BigDecimal("0" + num);
+  else{
+    this->value = BigInt(num);
+    this->scale = BigInt("0");
+  }
+};
+
+BigDecimal::BigDecimal(double num){
+  *this = BigDecimal(to_string(num));
+};
+
+BigDecimal::BigDecimal(BigInt v, BigInt s){
+  this->value = v;
+  this->scale = s;
+};
+
+BigDecimal::~BigDecimal(){
+  this->value = BigInt();
+  this->scale = BigInt();
+};
+
+void BigDecimal::trim(){
+  BigInt one = BigInt("1");
+  BigInt ten = BigInt("10");
+  BigInt remainder = this->value % ten;
+  while(remainder.size() == 1 && remainder.back() == 0){
+    this->value = this->value / ten;
+    this->scale = this->scale - one;
+    remainder = this->value % ten;
+  }
+};
+
+unsigned long int BigDecimal::precision(){
+  string v = to_string(this->value);
+  if(v.find('-', 0) == 0) v = v.replace(0, 1, '');
+  return v.length();
+};
+
+//Getters
+BigInt BigInt::getValue(){
+  return this->value;
+}
+
+BigInt BigInt::getScale(){
+  return this->scale;
+}
+
+//Operators
+BigDecimal operator =(const BigDecimal& a){
+  this->value = a.getValue();
+  this->scale = a.getScale();
+  this->trim();
+};
+
+ostream& operator <<(ostream& os, const BigDecimal& a){
+  BigDecimal bd = a;
+  BigInt v = bd.getValue();
+  BigInt s = bd.getScale();
+  BigInt zero = BigInt("0");
+  BigInt one = BigInt("1");
+  string val = "";
+  while(v.size() > 0){
+    val += to_string(v.pop_back());
+  }
+  if(v < zero){
+    os << "-";
+  }
+  if(s > zero){
+    os << "0.";
+    s = s - BigInt(to_string(bd.precision()));
+    while(s > zero){
+      os << "0";
+      s = s - one;
+    }
+    os << val;
+  }
+  else if(s < zero){
+    s = s + BigInt(to_string(bd.precision())));
+    for(BigInt i=zero; i<s; i=i+one){
+      os << val.front();
+      val.front() = '';
+    }
+    os << ".";
+    os << val;
+  }
+  else os << val;
+  return os;
+};
+
+void matchScales(BigDecimal& first, BigDecimal& second){
+  BigInt s_first = first.getScale();
+  BigInt s_second = second.getScale();
+  if(s_first < s_second) first = BigDecimal(first.getValue()*pow(BigInt("10"),s_second-s_first), s_second);
+  else if(s_first > s_second) second = BigDecimal(second.getValue()*pow(BigInt("10"),s_first-s_second), s_first);
+};
+
+BigDecimal operator +(const BigDecimal& first, const BigDecimal& second){
+  BigDecimal f_add = first;
+  BigDecimal s_add = second;
+  matchScales(f_add, s_add);
+  BigInt add_value = f_add.getValue() + s_add.getValue();
+  BigDecimal addition = BigDecimal(add_value, first.getScale());
+  return addition;
+};
+
+BigDecimal operator -(const BigDecimal& first, const BigDecimal& second){
+  BigDecimal f_sub = first;
+  BigDecimal s_sub = second;
+  matchScales(f_sub, s_sub);
+  BigInt sub_value = f_sub.getValue() - s_sub.getValue();
+  BigDecimal subtraction = BigDecimal(sub_value, first.getScale());
+  return subtraction;
+
+};
+
+BigDecimal operator *(const BigDecimal& first, const BigDecimal& second){
+  return BigDecimal((first.getValue()*second.getValue()), (first.getScale()+second.getScale());
+};
+
+bool operator <(const BigDecimal& first, const BigDecimal& second){
+  BigDecimal f = first;
+  BigDecimal s = second;
+  matchScales(f, s);
+  return f.getValue() < s.getValue();
+};
+
+bool operator >(const BigDecimal& first, const BigDecimal& second){
+  return !(first < second);
+};
+
+BigDecimal operator /(const BigDecimal& first, const BigDecimal& second){
+  BigDecimal f_div = first;
+  BigDecimal s_div = second;
+  matchScales(f_div, s_div);
+  unsigned long int max_prec = f_div.precision() + round(s_div.precision()*10/3);
+  BigDecimal ten_pow = BigDecimal(pow(BigInt("10"), max_prec), BigInt("0"));
+  if(f_div < s_div){
+    f_div2 = f_div * ten_pow;
+    BigInt div_v = f_div2.getValue() / s_div.getValue();
+    BigInt div_s = s_div.getScale() - (ten_pow.getScale() / BigInt("2"));
+    return BigDecimal(div_v, div_s);
+  else if(f_div > s_div){
+    s_div2 = s_div * ten_pow;
+    BigInt div_v = f_div.getValue() / s_div2.getValue();
+    BigInt div_s = f_div.getScale() - (ten_pow.getScale() / BigInt("2"));
+    return BigDecimal(div_v, div_s);
+  }
+};
+
+BigDecimal operator %(const BigDecimal& first, const BigDecimal& second){
+  BigDecimal zero = BigDecimal();
+  if(first.getScale() > zero || first.getScale() < zero) return zero;
+  else if(second.getScale() > zero || second.getScale() < zero) return zero; 
+  BigInt mod_v = first.getValue() % second.getValue();
+  return BigDecimal(mod_v, zero);
+};
+
+//n must be positive
+BigDecimal pow(const BigDecimal& a, const unsigned long int n){
+  BigDecimal one = BigDecimal("1");
+  if(n == 0) return one;
+  unsigned long int z = n;
+  BigDecimal x = a;
+  BigDecimal y = one;
+  while(z > 1){
+    if(z % 2 == 0){
+      x = x * x;
+      z = (unsigned long int)(z / 2);
+    }
+    else{
+      y = x * y;
+      x = x * x;
+      z = (unsigned long int)((z - 1) / 2);
+    }
+  }
+  return x * y;
+};
+
+//n must be positive
+BigDecimal pow(const BigDecimal& a, const BigInt& n){
+  BigDecimal one = BigDecimal("1");
+  if(n.size() == 1 && n.back() == 0) return one;
+  BigDecimal x = a;
+  BigInt y = one;
+  BigInt z = n;
+  BigInt mod = BigInt("1");
+  BigInt two = BigInt("2");
+  while(z > one){
+    mod = z % two;
+    if(mod.size() == 1 && mod.back() == 0){
+      x = x * x;
+      z = z / two;
+    }
+    else{
+      y = x * y;
+      x = x * x;
+      z = (z - one) / two;
+    }
+  }
+  return x * y;
+};
