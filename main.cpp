8b36a98f409387324c4127c443a638e2b5af406c
Ian Jarvis <ijarvis12@gmail.com>
2023-09-14 16:55:23 -0500

    Update main.cpp
    operator tests for bigdecimal

--- main.cpp
+++ main.cpp
@@ -49,8 +49,28 @@ int main(){
   BigInt t5 = BigInt("9999999999999999") % BigInt("-3333333333");
   cout << t5 << endl;
 
-  BigInt t6 = pow(BigInt("2"),36);
+  BigInt t6 = pow(BigInt("2"), 36);
   cout << t6 << endl;
+
+  cout << endl << "Operator Tests for BigDecimal" << endl;
+
+  BigDecimal t7 = BigDecimal("0.1") + BigDecimal("0.01");
+  cout << t7 << endl;
+
+  BigDecimal t8 = BigDecimal("0.1") - BigDecimal("0.01");
+  cout << t8 << endl;
+
+  BigDecimal t9 = BigDecimal("0.00625") * BigDecimal("0.002");
+  cout << t9 << endl;
+
+  BigDecimal t10 = BigDecimal("1") / BigDecimal("16");
+  cout << t10 << endl;
+
+  BigDecimal t11 = BigDecimal("36") % BigDecimal("2");
+  cout << t11 << endl;
+
+  BigDecimal t12 = pow(BigDecimal("0.1"), 36);
+  cout << t12 << endl;
 
   return 0;
 }
