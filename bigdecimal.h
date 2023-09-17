f6657c06807d5691fd258db0f8cb65f37446cfe2
Ian Jarvis <ijarvis12@gmail.com>
2023-09-17 12:20:25 -0500

    Update bigdecimal.h
    << operator update

--- bigdecimal.h
+++ bigdecimal.h
@@ -139,7 +139,7 @@ ostream& operator <<(ostream& os, const BigDecimal& a){
   if(s > prec){
     os << "0.";
     BigInt counter = s - prec;
-    while(counter > zero){
+    while(counter > one){
       os << "0";
       counter = counter - one;
     }
